c697c4a4c69a77722b948952512e99a28b943055
anna-rybina <annav.rybina@yandex.ru>
2025-02-20 21:13:38 +0300

    Add files via upload

---
+++ Homework2/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+
+        System.out.println("Итого (руб): " + (income - spending));
+
+    }
+}
