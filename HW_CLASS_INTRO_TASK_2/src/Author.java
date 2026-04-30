dfac0653c3c3877c4c9f8f76cb3cb3abbffdf8f9
anna-rybina <annav.rybina@yandex.ru>
2026-04-30 14:50:29 +0300

    Update Author.java

--- HW_CLASS_INTRO_TASK_2/src/Author.java
+++ HW_CLASS_INTRO_TASK_2/src/Author.java
@@ -1,13 +1,32 @@
 public class Author {
-
-    public String name;
-    public String surname;
-    public int rating;
+    private String name;
+    private String surname;
+    private int rating;
 
     public Author(String name, String surname, int rating) {
         this.name = name;
         this.surname = surname;
         this.rating = rating;
+
+    }
+
+    public boolean matches(String word) {
+        if (word == null) {
+            return false;
+        }
+        return name.contains(word) || surname.contains(word);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getRating() {
+        return rating;
     }
 }
 
