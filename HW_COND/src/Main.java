e88e69da59b8e67386ad653319a4ad73a0e30ec4
anna-rybina <annav.rybina@yandex.ru>
2026-05-07 16:02:43 +0300

    Update Main.java

--- HW_COND/src/Main.java
+++ HW_COND/src/Main.java
@@ -4,15 +4,28 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите год в формате yyyy");
-        int year = scanner.nextInt();
+        int counter = 0;
+        while (true) {
+            System.out.println("Введите год в формате yyyy:");
+            int year = scanner.nextInt();
+            System.out.println("Введите количество дней:");
+            int days = scanner.nextInt();
+            if (days == countDays(year)) {
+                counter++;
+            }
+            if (countDays(year) != days) {
+                System.out.println("Неправильно! В этом году " + countDays(year) + " дней");
+                System.out.println("Набрано очков: " + counter);
+                break;
+            }
+        }
+    }
 
-        if (year % 400 == 0) {
-            System.out.println("Количество дней в году: 366");
-        } else if ( (year % 4 == 0) && (year % 100 != 0) ) {
-            System.out.println("Количество дней в году: 366");
+    public static int countDays(int year) {
+        if (year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0))) {
+            return 366;
         } else {
-            System.out.println("Количество дней в году: 365");
-      }
+            return 365;
+        }
     }
 }
