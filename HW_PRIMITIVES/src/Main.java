9e52d685f83ba897b59c481c8dbc31251b46dd8c
anna-rybina <annav.rybina@yandex.ru>
2026-05-11 15:25:46 +0300

    Update Main.java

--- HW_PRIMITIVES/src/Main.java
+++ HW_PRIMITIVES/src/Main.java
@@ -1,83 +1,113 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-
-    public static int taxEarningsMinusSpendings(int earnings, int spendings) {
-        int tax1 = (earnings - spendings) * 15 / 100;
-        if (tax1 >= 0) {
-            return tax1;
-        } else {
-            // если расходы оказались больше, то налог посчитается отрицательным
-            return 0;
-        }
-    }
-
-    public static int taxEarnings(int earnings) {
-        int tax2 = earnings * 6 / 100;
-        if (tax2 >= 0) {
-            return tax2;
-        } else {
-            // если расходы оказались больше, то налог посчитается отрицательным
-            return 0;
-        }
-    }
+    private static final int RATE_INCOME = 6;
+    private static final int RATE_INCOME_MINUS_EXPENSES = 15;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int earnings = 0;
         int spendings = 0;
 
         while (true) {
-            System.out.println("Выберите операцию и введите её номер:");
-            System.out.println("1. Добавить новый доход");
-            System.out.println("2. Добавить новый расход");
-            System.out.println("3. Выбрать систему налогообложения");
+            System.out.println("""
+                    Выберите операцию и введите её номер:
+                    1. Добавить новый доход
+                    2. Добавить новый расход
+                    3. Выбрать систему налогообложения
+                    Для выхода введите end""");
+
             String input = scanner.nextLine();
-            if ("end".equals(input)) {
+            if ("end".equalsIgnoreCase(input)) {
                 break;
             }
 
-            int operation = Integer.parseInt(input);
+            int operation;
+
+            try {
+                operation = Integer.parseInt(input);
+            } catch (NumberFormatException  e) {
+                System.out.println("Введите 1, 2, 3 или end");
+                continue;
+            }
+
             switch (operation) {
                 case 1:
-                    System.out.println("Введите сумму дохода:");
-                    String moneyStr1 = scanner.nextLine(); // Не используйте тут nextInt (!)
-                    int money1 = Integer.parseInt(moneyStr1);
-                    earnings = earnings + money1;
+                    while (true) {
+                        System.out.println("Введите сумму дохода:");
+                        String earningsInput = scanner.nextLine();
+                        int earnedMoney;
+                        try {
+                            earnedMoney = Integer.parseInt(earningsInput);
+                            if (earnedMoney < 0) {
+                                System.out.println("Сумма не может быть отрицательной");
+                                continue;
+                            }
+                            earnings += earnedMoney;
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Введите число - сумму дохода");
+                        }
+
+                    }
                     break;
                 case 2:
-                    System.out.println("Введите сумму расхода:");
-                    String moneyStr2 = scanner.nextLine(); // Не используйте тут nextInt (!)
-                    int money2 = Integer.parseInt(moneyStr2);
-                    spendings = spendings + money2;
+                    while (true) {
+                        System.out.println("Введите сумму расхода:");
+                        String spendingsInput = scanner.nextLine();
+                        int spentMoney;
+                        try {
+                            spentMoney = Integer.parseInt(spendingsInput);
+                            if (spentMoney < 0) {
+                                System.out.println("Сумма не может быть отрицательной");
+                                continue;
+                            }
+                            spendings += spentMoney;
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Введите число - сумму расхода");
+                        }
+                    }
                     break;
                 case 3:
-                    int taxSystem1 = taxEarningsMinusSpendings(earnings, spendings);
-                    int taxSystem2 = taxEarnings(earnings);
-                    int economy1 = taxSystem1 - taxSystem2;
-                    int economy2 = taxSystem2 - taxSystem1;
-                    if (taxSystem1 > taxSystem2) {
-                        System.out.println("Мы советуем вам УСН доходы");
-                        System.out.println("Ваш налог составит: " + taxSystem2);
-                        System.out.println("Мы советуем вам УСН доходы");
-                        System.out.println("Налог на другой системе: " + taxSystem1);
-                        System.out.println("Экономия: " + economy1);
-                    } else if (taxSystem2 > taxSystem1) {
-                        System.out.println("Мы советуем вам УСН доходы минус расходы");
-                        System.out.println("Ваш налог составит: " + taxSystem1);
-                        System.out.println("Мы советуем вам УСН доходы");
-                        System.out.println("Налог на другой системе: " + taxSystem2 );
-                        System.out.println("Экономия: " + economy2);
-
-                    } else {
-                        System.out.println("Можете выбрать любую систему налогообложения");
-                    }
+                    int earningsTax = taxEarnings(earnings);
+                    int spendingsTax = taxEarningsMinusSpendings(earnings, spendings);
+                    showTaxComparison(earningsTax, spendingsTax);
                     break;
                 default:
                     System.out.println("Такой операции нет");
             }
         }
+
         System.out.println("Программа завершена!");
+
+    }
+
+    public static void showTaxComparison(int earningsTax, int spendingsTax) {
+        if (earningsTax > spendingsTax) {
+            System.out.println("Мы советуем вам УСН доходы минус расходы");
+            System.out.println("Ваш налог составит: " + spendingsTax + " рублей");
+            System.out.println("Налог на другой системе: " + earningsTax + " рублей");
+            System.out.println("Экономия: " + (earningsTax - spendingsTax) + " рублей");
+        } else if (earningsTax < spendingsTax) {
+            System.out.println("Мы советуем вам УСН доходы");
+            System.out.println("Ваш налог составит: " + earningsTax + " рублей");
+            System.out.println("Налог на другой системе: " + spendingsTax + " рублей");
+            System.out.println("Экономия: " + (spendingsTax - earningsTax) + " рублей");
+        } else if (earningsTax == spendingsTax) {
+            System.out.println("Можете выбрать любую систему налогообложения");
+            System.out.println("Налог на обеих системах: " + earningsTax + " рублей" );
+        }
+    }
+
+    public static int taxEarningsMinusSpendings(int earnings, int spendings) {
+        int tax = (earnings - spendings) * RATE_INCOME_MINUS_EXPENSES / 100;
+        return (tax>=0) ? tax : 0;
+    }
+
+    public static int taxEarnings (int earnings) {
+        int tax = earnings * RATE_INCOME / 100;
+        return (tax>=0) ? tax : 0;
     }
 }
