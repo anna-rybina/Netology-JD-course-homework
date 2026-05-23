2d57f7a93452be50cf5860f391e9f33d9438e869
anna-rybina <annav.rybina@yandex.ru>
2026-05-24 00:25:03 +0300

    Update Main.java

--- HW_ARR/src/Main.java
+++ HW_ARR/src/Main.java
@@ -1,49 +1,61 @@
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String[] products = {"Хлеб", "Яблоки", "Молоко"};
+        int[] prices = {100, 200, 300};
+        int[] basket = new int[products.length];
 
-        System.out.println("Список возможных товаров для покупки");
-        System.out.println("1. Молоко 50 руб/шт");
-        System.out.println("2. Хлеб 14 руб/шт");
-        System.out.println("3. Гречневая крупа 80 руб/шт");
+        showFoodMenu(products, prices);
+        fillBasket(basket);
+        showFullBasket(products, prices, basket);
 
-        String[] products = {"Молоко", "Хлеб", "Гречневая крупа"};
-        int[] prices = {50, 14, 80};
-        int[] productsAmount = new int[products.length];
-        int sumProducts = 0;
+    }
 
+    private static void showFullBasket(String[] products, int[] prices, int[] basket) {
+        System.out.println("Ваша корзина:");
+        int total = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (basket[i] > 0) {
+                int currentPrice = basket[i] * prices[i];
+                total += currentPrice;
+
+                System.out.printf("%s %d шт %d руб/шт %d руб в сумме\n",
+                        products[i],
+                        basket[i],
+                        prices[i],
+                        currentPrice);
+            }
+        }
+        System.out.printf("Итого %d руб\n", total);
+    }
+
+
+    public static void fillBasket (int[] basket) {
         while (true) {
-            System.out.println("Выберите товар и количество или введите `end`");
-            String input = scanner.nextLine();
-            if ("end".equals(input)) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите номер товара и количество или введите `end`");
+            String stringInput = scanner.nextLine(); // 2 10
+
+            if (stringInput.equalsIgnoreCase("end")) {
                 break;
             }
 
-            String[] parts = input.split(" ");
+            String[] parts = stringInput.split(" ");
             int productNumber = Integer.parseInt(parts[0]) - 1;
-            int numberOfProducts = Integer.parseInt(parts[1]);
+            int amountOfFood = Integer.parseInt(parts[1]);
+            basket[productNumber] += amountOfFood;
 
-            productsAmount[productNumber] = productsAmount[productNumber] + numberOfProducts;
+        }
+    }
 
-            int currentPrice = prices[productNumber]; 
-            sumProducts = sumProducts + (currentPrice * numberOfProducts); 
+    public static void showFoodMenu(String[] products, int[] prices) {
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < products.length; i++) {
+            System.out.printf("%d. %s %d руб/шт\n", i+1,  products[i], prices[i]);
         }
-        for (int productIndex = 0; productIndex < products.length; productIndex++) {
-            if (productsAmount[productIndex] == 0) {
-                continue;
-            }
-            System.out.println(products[productIndex]
-                    + " "
-                    + productsAmount[productIndex]
-                    + " шт "
-                    + prices[productIndex]
-                    + " руб/шт "
-                    + prices[productIndex] * productsAmount[productIndex]
-                    + " руб в сумме ");
-        }
-        System.out.println("Итого: " + sumProducts);
     }
 }
